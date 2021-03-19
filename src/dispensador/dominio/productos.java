e0dcbb289b16d2e1ea5883e3032a19e4347ae5da
1193090526alexander <elmerp1193090526@gmail.com>
2021-03-19 18:28:50 -0500

    maquina

---
+++ src/dispensador/dominio/productos.java
@@ -0,0 +1,41 @@
+package dispensador.dominio;
+
+public class productos {
+    private String codigo;
+    private String nombre;
+    private short precioMecato;
+    private byte maxCantidad;
+    private byte totalProductos;
+
+    public productos(String codigo, String nombre, short precioMecato) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precioMecato = precioMecato;
+        this.maxCantidad = 8;
+        this.totalProductos = 0;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public short getPrecioMecato() {
+        return precioMecato;
+    }
+
+    public byte getTotalProductos() {
+        return totalProductos;
+    }
+    public byte getMaxCantidad() {
+        return maxCantidad;
+    }
+
+    public void setPrecioMecato(short precio) {
+        this.precioMecato = precioMecato;
+    }
+
+}
